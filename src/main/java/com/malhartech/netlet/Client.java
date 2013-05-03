0796d2d43864b650729f3cac1c77b2f9dd60a392
Chetan Narsude <chetan@malhar-inc.com>
2013-05-03 15:12:06 -0700

    Moved the Fragment class out of Client.

--- src/main/java/com/malhartech/netlet/Client.java
+++ src/main/java/com/malhartech/netlet/Client.java
@@ -4,6 +4,7 @@
  */
 package com.malhartech.netlet;
 
+import com.malhartech.util.Fragment;
 import com.malhartech.netlet.Listener.ClientListener;
 import com.malhartech.util.CircularBuffer;
 import java.io.IOException;
@@ -282,61 +283,6 @@ public abstract class Client implements ClientListener
       }
 
     };
-  }
-
-  public static class Fragment
-  {
-    public byte[] buffer;
-    public int offset;
-    public int length;
-
-    public Fragment(byte[] array, int offset, int length)
-    {
-      buffer = array;
-      this.offset = offset;
-      this.length = length;
-    }
-
-    @Override
-    public int hashCode()
-    {
-      int hash = 5;
-      hash = 59 * hash + Arrays.hashCode(this.buffer);
-      hash = 59 * hash + this.offset;
-      hash = 59 * hash + this.length;
-      return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj)
-    {
-      if (obj == null) {
-        return false;
-      }
-      if (getClass() != obj.getClass()) {
-        return false;
-      }
-      final Fragment other = (Fragment)obj;
-      if (!Arrays.equals(this.buffer, other.buffer)) {
-        return false;
-      }
-      if (this.offset != other.offset) {
-        return false;
-      }
-      if (this.length != other.length) {
-        return false;
-      }
-      return true;
-    }
-
-    @Override
-    public String toString()
-    {
-      return "Fragment{" + (length > 256
-                            ? "buffer=" + buffer + ", offset=" + offset + ", length=" + length
-                            : Arrays.toString(Arrays.copyOfRange(buffer, offset, offset + length))) + '}';
-    }
-
   }
 
   private static final Logger logger = LoggerFactory.getLogger(Client.class);
