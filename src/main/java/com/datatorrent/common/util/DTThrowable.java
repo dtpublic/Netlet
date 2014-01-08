26331206450c8b17e2cfde179478335218d41dab
Chetan Narsude <chetan@datatorrent.com>
2014-01-08 15:54:12 -0800

    Added a helper method to properly rethrow the exceptions.

---
+++ src/main/java/com/datatorrent/common/util/DTThrowable.java
@@ -0,0 +1,28 @@
+/*
+ *  Copyright (c) 2012-2014 Malhar, Inc.
+ *  All Rights Reserved.
+ */
+package com.datatorrent.common.util;
+
+/**
+ * Helper method to ensure that exceptions are propertly thrown.
+ * If the cause is of type Error or RuntimeException then the
+ * cause it thrown as it is. Otherwise the cause is wrapped in
+ * a RuntimeException and the later is thrown.
+ */
+public class DTThrowable
+{
+  public static void rethrow(Throwable cause)
+  {
+    if (cause instanceof Error) {
+      throw (Error)cause;
+    }
+
+    if (cause instanceof RuntimeException) {
+      throw (RuntimeException)cause;
+    }
+
+    throw new RuntimeException(cause);
+  }
+
+}
